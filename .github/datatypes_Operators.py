90660741844ca7fd36be220400dd10d6fc410dbb
JXJ101 <s11886728@students.duvalschools.org>
2023-09-26 13:40:52 -0400

    saved into new game programming

---
+++ .github/datatypes_Operators.py
@@ -0,0 +1,87 @@
+# data types and operators, jahreem jeffers, v0.1
+# variable rules
+# CANNOT START WITH A NUMBER 
+# CANNOT USE BUILT-IN KEYBOARD AS VERIABLE 
+# VARIABLE NAME SHOULD DESCRIBE THE DATA STORED
+# SNAKE_CASE VARIABLE USE_ TO SEPERATE WORDS, ALL LOWER CASE
+# CAMELCASE VAIRIABLE DO NOT USE SPACES, 1ST WORD LOWER, REST UPPERCASE
+# STRING LITERAL EXAMPLES 
+PLAYERNAME = "JXJ"
+emptysctring = ""
+spacestring = " "
+monstername = "mordax"
+# integer data types
+health = "4000"
+damage = "99999"
+speed = "100000"
+# flpating point data types
+pi = 3.1415678
+velocity = 3.0
+gravity = 9000
+#boolean Data_Types 
+isFireTypes = False
+weaponEquipped = True
+pvpenable= True
+# arithmetic operators
+# PENDAS APPLIES JUST LIEK IN MATH CLASS
+print (3+4) #addition
+print (0 - 29) #subtraction
+print (3 * 4) #multiplication
+print (6 / 3) #division
+print (2 ** 14) #exponets
+print (82 / 3) #modulus, divide 2 numbers and gives remainder
+#comparison Operators
+# Evaluate the expression then return True or False
+print(3>2) # Greater Than
+print (7 >= -2) # Greater Than or Equal to
+print (-9 < -1) # Less Than
+print (0 <= 0) # Less Than or Equal To 
+print (2020 == 2021) # Is Equal To 
+print (-100 != 100) # Not Equal To
+
+# Assignment Operators 
+myVariable = "myValue" # assign variable on left value on the right, throw out any current value
+myOtherVariable = (60 + 9)
+
+myVariableAgain = 1 
+myVariableAgain = 5
+print(myVariableAgain)
+
+# Addition Assignment - - Add the value on the right, keeping any current vlaue
+myWallet = 0
+myWallet += 1
+myWallet += 5 
+print(myWallet)
+
+# Same Effect
+X = 0
+X += 1
+X = X + 1
+
+# Logical Operators
+print( 3 > 5 and 4 < 3) # AND requiers ALL expersions to be True.
+print(3 > 2 and 4 != 3)
+print( 3 > 2 and 4 != 3 and "favColor" == "Blue" and temp == -5)
+# When writing and expressions, put the value most likely to be False first!
+
+# Logical OR --  Requires ONE expression to be True 
+
+Print (5 > 2 or 2 < -2) 
+
+Print (3 != 3 or 5== 5) 
+
+ 
+
+# AND  OR Combined 
+
+Print(“Line 81”) 
+
+Print (3 > 2 and 4 < 3 or 5 != 7) 
+
+# Print (True and False or True) 
+
+# NOT Logical Operator  
+
+Print (not ( 3 >2)) 
+
+Print (not (not (not (5 != 3)))) 
---
+++ .github/numGuess.py
@@ -0,0 +1,111 @@
+# Select the secret number from a given range. 
+# Player must guess the number.
+# Compare guess to the secret number.
+# What happens if the guess is wrong?
+    # Tell them the guess is wrong. 
+    # Tell them the number of guesses left.
+    # Tell them if too high / too low.
+# What happens if the guess is right?
+    # Tell them guess is correct.
+    # Award a point. 
+# What happens if the player runs out of guesses?
+    # Tell player round has been lost.
+    # Award point to CPU.
+# Check to see if player or CPU has >= 3 points, if so they win. 
+import random # Import the random module to our code.
+
+    
+
+# DECLARATIONS 
+secretNumber = -1
+playerGuess = -1
+playerScore = 0
+cpuScore = 0
+numGuesses = 1 # How many guesses they have taken. 
+playerName = ""
+difficulty = ""
+rangeMin = -1
+rangeMax = -1 
+numAttempts = 4 # How many guesses they are allowed to have.  
+
+print("""
+        *~~~~~~~~~~~~~~~~~~~~~~~*
+        |     Guess  a Number   |
+        |         JXJ           |
+        |                       |
+        |        2023           |
+        *~~~~~~~~~~~~~~~~~~~~~~~*
+
+      """)
+# CPU SECRET NUMBER GENERATION
+
+secretNumber = random.randint(rangeMin,rangeMax)
+    
+    
+
+# GAMELOOP
+print("You need to guess a number from 0 to 20 and you have four guesses. \nIf you guess it right, you get a point. \nIf you get it wrong you lose a guess. \n lose all guesses the CPU get a point")
+# ADD CODE HERE TO CHANGE DIFFICULTY BETWEEN EACH MATCH
+# print() an explanation of your three difficulty levels.
+# Use input () to store difficulty in difficulty variable
+# assign values to numAttempts, rangeMin, and rangeMax based on choice.
+#input(1)
+
+while playerScore != 3  and cpuScore != 3:
+difficulty = input("type what difficulty you want to choose")
+print(f"you chose {difficulty} is that correct?")
+isCorrect = input("Please type yes if correct, no if not correct.\n")
+if isCorrect == "yes":
+    print(f"Ok {playerName}, let's continue!")
+
+    print(f"Player Score: {playerScore}\nCPU: {cpuScore}.\n")
+    secretNumber = random.randint(rangeMin, rangeMax)
+    # print(secret_number)
+    # You do not ask the player to input a difficulty.  
+    if difficulty == "easy": 
+        rangeMin = 0 
+        rangeMax = 10
+        numAttempts = 4
+    elif difficulty == "medium":
+        rangeMin = 0
+        rangeMax = 30
+        numAttempts = 4
+    elif difficulty == "hard":
+        rangeMin = 0
+        rangeMax = 50
+        numAttempts = 2
+    else: 
+        pass
+        # playerName = input("choose the difficulty\n Type the difficulty you want to choose and press Enter.\n")
+        # Print that the difficulty could not be matched, provide default values.
+        
+
+
+    
+    
+    for guesses in range(numAttempts): # What can you replace the 4 with to make the number of guesses change to reflect the difficulty?  
+        print(f"You have {numAttempts - numGuesses} guesses remaining\n")
+        playerGuess = int(input("Type a number from 0 to 20 and press ENTER.\n"))
+        # input() saves all data as a STRING by default
+        # int() will convert to a integer
+        # float() will convert to a FLOAT
+        print(f"You have chosen {playerGuess}. Let's see if you're right!\n")
+        if playerGuess == secretNumber:
+            playerScore  += 1 
+            print("Whoa dude, what a guess! You got it!\n")
+            break # IMMEDIATELY EXIT ANY LOOP YOU ARE IN!
+        else:
+            print("You did not guess correctly. Idiot.\n")
+            if playerGuess > secretNumber:
+                print("Your guess is too high.\n")
+            else:
+                print("Your guess is too low.\n")
+        numGuesses += 1
+    if playerGuess != secretNumber:
+        cpuScore += 1 
+        print("The CPU wins a point since you ran out of guesses.\n")
+        
+    if playerScore >3:
+        print("Winner, winner, chicken dinner! You scored 3 points first!\n")
+    else:
+        print("Yo, you lost to a computer. You are a scrub.\n")
