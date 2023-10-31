c239b3b472937a6d292246c96d22c92f5603c3a7
JXJ101 <142345298+JXJ101@users.noreply.github.com>
2023-10-31 14:34:27 -0400

    Update hangman.py

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,4 +1,5 @@
 # Hangman Game by jahreem Jeffers v0.1
+import random
 
 words = 'tricky hank pico whitty tabi agoti matt sans paparus gaster steve alex pepinno aiden jaiden chrisly blaze melina jason max rex chara doomslayer kratos thor madmax void carol hex johnnycage'.split()
 
@@ -41,7 +42,14 @@ HANGMAN_BOARD = ['''
     / \ |
      ========''']
      
+# Pick Word from List
+def getRandomWord(wordList): # Return a random word from the list.
+    wordIndex = random.randint(0,len(wordList)- 1)
+    #len(listName - 1 is EXTREMELY COMMON FOR WORKING FOR LISTS.
+    return wordList[wordIndex]
+    
 i = 0
-while i < len(HANGMAN_BOARD):
-    print(HANGMAN_BOARD[i])
+while i < 100:
+    word = getRandomWord(words)
+    print(word)
     i += 1
