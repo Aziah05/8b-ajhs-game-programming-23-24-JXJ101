8f0b02e0ad870d7009f37c436353273d04aeab96
JXJ101 <142345298+JXJ101@users.noreply.github.com>
2023-11-06 14:49:09 -0500

    Update hangman.py

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,7 +1,16 @@
 # Hangman Game by jahreem Jeffers v0.1
 import random
 
-words = 'tricky hank pico whitty tabi agoti matt sans paparus gaster steve alex pepinno aiden jaiden chrisly blaze melina jason max rex chara doomslayer kratos thor madmax void carol hex johnnycage'.split()
+#words = 'tricky hank pico whitty tabi agoti matt sans paparus gaster steve alex pepinno aiden jaiden chrisly blaze melina jason max rex chara doomslayer kratos thor madmax void carol hex johnnycage'.split()
+# DICTIONARY VERSION
+# Stored in Key:Value Pairs.
+# Actual Dictionary Woes (Key) : Value (Definition)
+# Uses {} to specify a dictionary
+words = {'Colors': 'Red orange yellow Green yellow blue indigo violet fuchia teal garnet gold black white silver gray'.split(),
+        'Animals': 'cat cow dog moose goose fish wombat wolverine giraffe wolf liom alligator'.split(),
+        'Shapes': 'square triangle circle rhombus parallogram trapeziod diamond dodecahedron'.split(),
+        'Foods': 'hamburger hotdog potato waffle pancake escargot oysters chips steak'.split()}
+
 
 # VARIABLE_NAME in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
 HANGMAN_BOARD = ['''
@@ -40,13 +49,30 @@ HANGMAN_BOARD = ['''
      0  |
     /|\ |
     / \ |
+     ========''', '''
+     +---+
+     0  |
+   o-|-o|
+    / \ |
+     ========''', '''
+     +---+
+     0   |
+   o-|-o |
+    / \  |
+   o   o |
      ========''']
      
 # Pick Word from List
-def getRandomWord(wordList): # Return a random word from the list.
-    wordIndex = random.randint(0,len(wordList)- 1)
+#def getRandomWord(wordList): # Return a random word from the list.
+    #wordIndex = random.randint(0,len(wordList)- 1)
     #len(listName - 1 is EXTREMELY COMMON FOR WORKING FOR LISTS.
-    return wordList[wordIndex]
+    #return wordList[wordIndex]
+ # Pick Word from Dictionary
+ 
+def getRandomWord(wordDict): # Return a random word from the list.
+    wordKey = random.choice(list(wordDict.keys()))
+    wordIndex = random.randint(0,len(wordDict[wordKey]) - 1)
+    return [wordDict[wordKey][wordIndex], wordKey]
     
 def displayBoard(missedLetters, correctLetters, secretWord):
     print(HANGMAN_BOARD[len(missedLetters)])
@@ -89,13 +115,28 @@ def playAgain():
 
 # Introduce the Game
 print('Welcome to Hangman by your host JXJ!')
+# CHOOSE DIFFICULTY
+difficulty = 'X'
+while difficulty not in 'EMH':
+    print('Please Choose Easy, Medium or Hard. Type the first letter then press enter.\n')
+    difficulty = input().upper()
+if difficulty == 'M': #medium
+    del HANGMAN_BOARD[8]
+    del HANGMAN_BOARD[7]
+if difficulty == 'H': # Hard
+    del HANGMAN_BOARD[8]
+    del HANGMAN_BOARD[7]
+    del HANGMAN_BOARD[5]
+    del HANGMAN_BOARD[3]
+
 missedLetters = ''
 correctLetters = ''
-secretWord = getRandomWord(words)
+secretWord, secretSet = getRandomWord(words)
 gameIsDone = False
 
 # Main Game Loop
 while True:
+    print('The secret word is from the ' + secretSet + ' category.\n')
     displayBoard(missedLetters, correctLetters, secretWord)
     
     guess = getGuess(missedLetters + correctLetters)
@@ -107,12 +148,12 @@ while True:
         foundAllLetters = True
         for i in range(len(secretWord)):
             if secretWord[i] not in correctLetters:
-                foundAllLetters = false
+                foundAllLetters = False
                 break
-            if foundAllLetters: # if True: 
-                print('congrats, you have won this time.')
-                print('The secret word was' + secretWord)
-                gameIsDone = True
+        if foundAllLetters: # if True: 
+            print('congrats, you have won this time.')
+            print('The secret word was' + secretWord)
+            gameIsDone = True
     else:
         missedLetters = missedLetters + guess
         
@@ -123,14 +164,14 @@ while True:
         print('The secret word was' + secretWord)
         gameIsDone = True
 
-if gameIsDone:
-    if playAgain(): 
-        missedLetters = ''
-        correctLetters = ''
-        gameIsDone = False
-        secretWord = getRandomWord(words)
-    else:
-        break
+    if gameIsDone:
+        if playAgain(): 
+            missedLetters = ''
+            correctLetters = ''
+            gameIsDone = False
+            secretWord, secretset = getRandomWord(words)
+        else:
+            break
 
 
 #i = 0
