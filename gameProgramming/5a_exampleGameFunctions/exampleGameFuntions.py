09209cfb0bc17d705d442ba0f2e29ebf4b7b2656
JXJ101 <142345298+JXJ101@users.noreply.github.com>
2023-11-13 13:51:24 -0500

    Update exampleGameFuntions.py

--- gameProgramming/5a_exampleGameFunctions/exampleGameFuntions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFuntions.py
@@ -1,1 +1,14 @@
 # Example Game Functions Project, Jahreem Jeffers, v0.0
+import random
+
+def functionaone():
+    pass
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "Default Value"):
+    pass
+
+def functionFour(param1, param2, param3):
+    pass
